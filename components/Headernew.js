447472580338ddee4d2c8105f6743f0a37fee0c4
muneeswaran <muneeswaran@eywamedia.com>
2025-12-24 15:56:42 +0530

    header

--- components/Headernew.js
+++ components/Headernew.js
@@ -1778,14 +1778,22 @@ const shouldShowArrow = (item, allItems = []) => {
               bg-white/10
               backdrop-blur-md
               border border-white/30">
-            {categories.map((cat) => (
+            {/* {categories.map((cat) => (
               <span
                 key={cat.category_slug}
                 className="text-sm text-gray-200 hover:text-[#1688c8] cursor-pointer"
               >
                 {cat.category_name}
               </span>
-            ))}
+            ))} */}
+            {categories?.slice(0, 4).map((cat) => (
+  <span
+    key={cat.category_slug}
+    className="text-sm text-gray-200 hover:text-[#1688c8] cursor-pointer"
+  >
+    {cat.category_name}
+  </span>
+))}
             <span className="bg-sky-500 text-xs px-3 py-1 rounded-full font-medium">
               Xmas & New year Sale
             </span>
