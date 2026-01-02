d72f99b43a6e8006f557215878bf5e2c81da6d7e
muneeswaran <muneeswaran@eywamedia.com>
2026-01-02 17:51:09 +0530

    header_mobile_view_changes

--- components/Headernew.js
+++ components/Headernew.js
@@ -1597,7 +1597,7 @@ const shouldShowArrow = (item, allItems = []) => {
 
 
       {/* Main Header bg-white */}
-                  <div className={`${isMobileMenuOpen ? "fixed inset-0 mt-0 pt-0 z-50 overflow-y-auto" : "px-4 sm:px-6 md:px-0 py-0 sticky top-0 z-40"}`} style={{ backgroundColor: "#424242" }}>
+                  <div className={`${isMobileMenuOpen ? "fixed inset-0 mt-0 pt-0 z-50 overflow-y-auto" : "px-4 sm:px-6 md:px-0 py-0 sticky top-0 z-40"}`} style={{ backgroundColor: "black" }}>
                       {/* NEW MOBILE TOP ROW (from reference) */}
                       <div className="sm:hidden flex items-center justify-between w-full relative">
                           <Link href="/" className="p-1 rounded-lg">
@@ -1660,11 +1660,10 @@ const shouldShowArrow = (item, allItems = []) => {
                           </div>
                       </div>
                       {/* NEW MOBILE SEARCH BAR */}
-                      <div className="sm:hidden mt-2 -mx-4 px-0">
+                      {/* <div className="sm:hidden mt-2 -mx-4 px-0">
                         <div className="bg-[#2453D3] w-full px-3 py-3">
                           <div className="flex items-center bg-white h-12 rounded-xl border border-gray-300 shadow-sm overflow-hidden w-full transition-all duration-150 focus-within:border-[#2453d3] focus-within:shadow-[0_0_0_2px_rgba(36,83,211,0.15)] flex-nowrap">
                             
-                            {/* Category select */}
                             <select
                               value={selectedCategory}
                               onChange={(e) => setSelectedCategory(e.target.value)}
@@ -1716,7 +1715,7 @@ const shouldShowArrow = (item, allItems = []) => {
                             </button>
                           </div>
                         </div>
-                      </div>
+                      </div> */}
                       {/* MOBILE TOP SUGGESTIONS (outside menu) */}
                       {searchDropdownVisible && searchContext === 'mobileTop' && !isMobileMenuOpen && (
                         <div ref={searchDropdownRef} className="sm:hidden absolute z-[70] left-0 right-0 px-3 mt-1">
@@ -1762,7 +1761,7 @@ const shouldShowArrow = (item, allItems = []) => {
 
       {/* ================= SEARCH BAR ROW ================= */}
       {/* <div className="bg-gradient-to-r from-[#1688C8] to-[#33a7b5]"> */}
-      <div className="bg-black">
+      <div className="hidden sm:grid  bg-black">
         <div className="max-w-7xl mx-auto px-4 py-4 flex items-center gap-4">
           <Link href="/" className="text-lg font-semibold">
            <img src="/user/unilet-logo.webp" alt="Logo" width={100} height={70} className="h-auto" />
