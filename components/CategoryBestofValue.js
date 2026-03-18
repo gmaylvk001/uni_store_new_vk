1d6338e8d927f776904653e615c8df8cde7c6c16
Abitha <ramesh@eywamedia.com>
2026-03-18 13:00:16 +0530

    change lg logo

--- components/CategoryBestofValue.js
+++ components/CategoryBestofValue.js
@@ -393,10 +393,11 @@ const scrollRight = (categoryId) => {
                                           {/* 
                                          <h4 className={`text-[10px] sm:text-xs mb-2 ${product.stock_status === "In Stock" ? "text-green-600" : "text-red-600"}`}>
                                            {product.stock_status}{product.stock_status === "In Stock" && product.quantity ? `, ${product.quantity} units` : ""}
-                                         </h4> */}
+                                         </h4> 
                                          <h4 className={`text-[10px] sm:text-xs mb-2 ${product.stock_status === "In Stock" ? "text-green-600" : "text-red-600"}`}>
                                            {product.stock_status}{product.stock_status === "In Stock" }
                                          </h4>
+                                         */}
   </Link>
                                           {/* Actions */}
                                           {/* <div
