0b3c80fc114720f87a5517b42263e273598972de
Abitha <ramesh@eywamedia.com>
2026-03-26 12:21:08 +0530

    remove related products from product page

--- app/product/[slug]/ProductClient.js
+++ app/product/[slug]/ProductClient.js
@@ -2101,6 +2101,7 @@ const fetchBrand = async () => {
     </div>
   </div>
 )} */}
+{/*
 {relatedProducts?.filter(item => item.stock_status === "In Stock").length > 0 && (
 <div className="px-4 py-4 border border-gray-300 rounded-lg shadow-md bg-white max-h-[500px] overflow-y-scroll scrollbar-hide">
         <h2 className="text-sm font-bold text-black underline mb-2">
@@ -2178,6 +2179,7 @@ const fetchBrand = async () => {
           ))}
       </div>
   )}
+  */}
    {/* <div className="px-4 py-4">
         <h2 className="text-sm font-bold text-customBlue underline mb-2">
           Similar Products
