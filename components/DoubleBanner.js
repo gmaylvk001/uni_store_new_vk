101e3675ec389a1f651a7fc05469441c96c9d4a8
Ragul <ragulempire2kk3@gmail.com>
2026-04-02 17:53:22 +0530

    Added and modified Some Banners

--- components/DoubleBanner.js
+++ components/DoubleBanner.js
@@ -6,21 +6,21 @@ const DoubleBanner = () => {
   const banners = [
     {
       id: 1,
-      img: "uploads/aboutus/SamsungTV.png",
-      link: "/category/tv-entertainment",
-      alt: "TV Sale Hot Offers",
+      img: "uploads/aboutus/Fridge-bannerr.jpeg",
+      link: "/category/home-appliances/refrigerators",
+      alt: "Fridge Sale Hot Offers",
     },
     {
       id: 2,
-      img: "uploads/aboutus/GodrejAc.png",
-      link: "/category/home-appliances/air-conditioner",
-      alt: "Godrej AC Sale",
-    },
-    {
-      id: 3,
-      img: "uploads/aboutus/Apple-banner.jpeg",
+      img: "uploads/aboutus/Apple-banner-square.jpeg",
       link: "/category/mobiles-accessories/mobile-phones/iphone",
       alt: "New iPhone Launch",
+    },
+     {
+      id: 3,
+      img: "uploads/aboutus/Ac-Banner-Square.png",
+      link: "/category/home-appliances/air-conditioner",
+      alt: "Godrej AC Sale",
     },
   ];
 
@@ -28,12 +28,12 @@ const DoubleBanner = () => {
     <section className="px-4 md:px-8 py-4 w-full">
       <div className="max-w-screen mx-auto flex flex-col md:flex-row gap-3 lg:gap-4">
 
-        {/* Left - Large Image */}
+      
         <Link
           href={banners[0].link}
-          className="block overflow-hidden rounded-lg shadow-sm hover:shadow-md transition-shadow md:w-1/2"
+          className="block overflow-hidden rounded-lg shadow-sm hover:shadow-md transition-shadow md:w-4/5"
         >
-          <div className="relative w-full h-[200px] sm:h-[260px] md:h-[320px] lg:h-[350px]">
+          <div className="relative w-full h-[135px] sm:h-[260px] md:h-[320px] lg:h-[350px]">
             <img
               src={banners[0].img}
               alt={banners[0].alt}
@@ -42,15 +42,15 @@ const DoubleBanner = () => {
           </div>
         </Link>
 
-        {/* Right - Two Stacked Images */}
-        <div className="flex flex-row md:flex-col gap-3 lg:gap-4 md:w-1/2">
+        <div className="flex flex-row gap-3 lg:gap-4 md:w-2/5">
           {[banners[1], banners[2]].map((banner) => (
             <Link
               href={banner.link}
               key={banner.id}
               className="block overflow-hidden rounded-lg shadow-sm hover:shadow-md transition-shadow flex-1"
             >
-              <div className="relative w-full h-[100px] sm:h-[125px] md:h-[154px] lg:h-[166px]">
+              {/* Height perfectly matches the left image */}
+              <div className="relative w-full h-[180px] sm:h-[260px] md:h-[320px] lg:h-[350px]">
                 <img
                   src={banner.img}
                   alt={banner.alt}
